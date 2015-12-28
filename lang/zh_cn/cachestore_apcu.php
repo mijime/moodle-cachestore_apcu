00f35f0a4fb69acb539620f763900f82cd381ddb
Linnaea Von Lavia <le.concorde.4590@gmail.com>
2015-12-28 16:30:09 +0800

    zh_cn language

---
+++ lang/zh_cn/cachestore_apcu.php
@@ -0,0 +1,27 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * APCu cache store language strings.
+ *
+ * @package    cachestore_apcu
+ * @copyright  2012 Sam Hemelryk
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'APC键值缓存 (APCu)';
+$string['testperformance'] = '测试性能';
+$string['testperformance_desc'] = '启用后将在访问性能测试页时测试APCu缓存性能，不推荐在生产服务器上启用。';
